1ffbf14ddc87b082681d4a0d6e5a19243344f31b
zwq <1623754384@qq.com>
2018-07-19 21:44:14 +0800

    fff

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ App({
   globalData: {
     // urlHeaderA:'http://192.168.1.136:1007/phoneCard/v1/noIntercept/',
     urlHeaderA: 'https://app-group.lihetianxia.com/phoneCard/v1/noIntercept/',
-    urlHeaderB: 'https://jllt.linplus.com.cn/test/',
-    urlHeaderC: 'http://mob3.6lidi.com/',
+    urlHeaderB: 'https://jllt.linplus.com.cn/',
+    urlHeaderC: 'https://jllt.linplus.com.cn/',
     timer: null,
     h: '00',
     m: '00',
